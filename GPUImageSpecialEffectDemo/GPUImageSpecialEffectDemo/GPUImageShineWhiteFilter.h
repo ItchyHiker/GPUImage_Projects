8d6687688f19550b98659f0f45251da3854f1614
vincent <vincentcheng929@gmail.com>
2020-07-13 17:57:15 +0800

    Add shine white filter

---
+++ GPUImageSpecialEffectDemo/GPUImageSpecialEffectDemo/GPUImageShineWhiteFilter.h
@@ -0,0 +1,17 @@
+//
+//  GPUImageShineWhiteFilter.h
+//  GPUImageSpecialEffectDemo
+//
+//  Created by yuhua.cheng on 2020/7/13.
+//  Copyright © 2020 idealabs. All rights reserved.
+//
+
+#import "GPUImageFilter.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GPUImageShineWhiteFilter : GPUImageFilter
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GPUImageSpecialEffectDemo/GPUImageSpecialEffectDemo/GPUImageShineWhiteFilter.m
@@ -0,0 +1,13 @@
+//
+//  GPUImageShineWhiteFilter.m
+//  GPUImageSpecialEffectDemo
+//
+//  Created by yuhua.cheng on 2020/7/13.
+//  Copyright © 2020 idealabs. All rights reserved.
+//
+
+#import "GPUImageShineWhiteFilter.h"
+
+@implementation GPUImageShineWhiteFilter
+
+@end
