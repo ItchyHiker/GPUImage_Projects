4f33e9598ecee596edf319fb0c7d8c354ec346ad
vincent <vincentcheng929@gmail.com>
2020-07-13 17:57:35 +0800

    Add vertigo filter

---
+++ GPUImageSpecialEffectDemo/GPUImageSpecialEffectDemo/GPUImageVertigoFilter.m
@@ -0,0 +1,13 @@
+//
+//  GPUImageVertigoFilter.m
+//  GPUImageSpecialEffectDemo
+//
+//  Created by yuhua.cheng on 2020/7/13.
+//  Copyright © 2020 idealabs. All rights reserved.
+//
+
+#import "GPUImageVertigoFilter.h"
+
+@implementation GPUImageVertigoFilter
+
+@end
